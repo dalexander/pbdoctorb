b39afbe3698067dbe8299497196dd88b47817b1f
David Alexander <dalexander@pacificbiosciences.com>
2014-08-28 09:48:04 -0700

    The override mechanism described in the spec has a pretty major limitation: in a cmp.h5 there is no way to override some movies but not others.
    Additionally the current loadChemistry implementation does not honor
    chemistry overrides in the bas/bax files---this could be fixed but
    then we have to deal with the problem above somehow.
    
    Override using the barcode triples in the h5 files.  The original
    chemistry information can always be retrieved from the metadata.xml
    file.

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'pbdoctorb',
+    version = "0.1.0",
     author='Pacific Biosciences',
     author_email='devnet@pacificbiosciences.com',
     license=open("LICENSES.txt").read(),
