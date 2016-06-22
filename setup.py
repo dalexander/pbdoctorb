022458cc6593784c8a50a262d9c52d4d23d300f8
David Alexander <dalexander@pacificbiosciences.com>
2016-06-22 16:19:49 -0700

    Version 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'pbdoctorb',
-    version = "0.2.0",
+    version = "0.3.0",
     author='Pacific Biosciences',
     author_email='devnet@pacificbiosciences.com',
     license=open("LICENSES.txt").read(),
